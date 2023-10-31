83f7d6a437a999ef6eb546e51b8ae6aa6555227c
kr805566 <62494588+kr805566@users.noreply.github.com>
2023-11-01 07:00:35 +0800

    Add files via upload

---
+++ 艾克曼函數/艾克曼函數/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Function 遞迴(ByVal m As Integer, ByVal n As Integer) As Integer
+        If m = 0 Then
+            遞迴 = n + 1
+        ElseIf n = 0 Then
+            遞迴 = 遞迴(m - 1, 1)
+        Else
+            遞迴 = 遞迴(m - 1, 遞迴(m, n - 1))
+        End If
+    End Function
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox3.Text = 遞迴(Val(TextBox1.Text), Val(TextBox2.Text))
+    End Sub
+
+ 
+End Class
---
+++ 費式數列/費式數列/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Function 遞迴(ByVal n As Integer)
+        If n = 1 Or n = 2 Then 遞迴 = 1 Else 遞迴 = 遞迴(n - 1) + 遞迴(n - 2)
+
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox2.Text = 遞迴(Val(TextBox1.Text))
+    End Sub
+End Class
