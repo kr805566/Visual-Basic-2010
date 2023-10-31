7a9d431c58fe72c8e58d08e57c297549ddca2f97
kr805566 <62494588+kr805566@users.noreply.github.com>
2023-11-01 06:57:56 +0800

    Add files via upload

---
+++ 數字漣漪/01234543210/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("新細明體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(9, 20)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(444, 406)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("新細明體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(39, 458)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 30)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "9"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(320, 458)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "輸入"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(471, 535)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ 數字漣漪/01234543210/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim n As Integer
+        n = Val(TextBox2.Text)
+        TextBox1.Text = ""
+        For i = 0 To n
+            TextBox1.Text = TextBox1.Text & Space((n - i) * 2)
+            For j = 0 To i
+                TextBox1.Text = TextBox1.Text & j
+            Next j
+
+            For j = i - 1 To 0 Step -1
+                TextBox1.Text = TextBox1.Text & j
+            Next j
+            TextBox1.Text = TextBox1.Text & vbCrLf
+        Next i
+
+
+        For i = n - 1 To 0 Step -1
+            TextBox1.Text = TextBox1.Text & Space((n - i) * 2)
+            For j = 0 To i
+                TextBox1.Text = TextBox1.Text & j
+            Next j
+
+            For j = i - 1 To 0 Step -1
+                TextBox1.Text = TextBox1.Text & j
+            Next j
+            TextBox1.Text = TextBox1.Text & vbCrLf
+        Next i
+       
+
+    End Sub
+End Class
