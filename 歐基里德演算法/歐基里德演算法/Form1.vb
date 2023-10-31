53438d4743a577e5e9d01ded82a92d71bfe4085b
kr805566 <62494588+kr805566@users.noreply.github.com>
2023-11-01 07:02:10 +0800

    Add files via upload

---
+++ 歐基里德演算法/歐基里德演算法/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Call 遞迴(Val(TextBox1.Text), Val(TextBox2.Text))
+    End Sub
+
+
+    Function 遞迴(ByVal m As Integer, ByVal n As Integer)
+        If n = 0 Then TextBox3.Text = m Else Call 遞迴(n, m Mod n)
+
+
+    End Function
+End Class
