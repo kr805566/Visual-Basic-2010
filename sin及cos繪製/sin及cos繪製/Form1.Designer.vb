6aca85baed40869966a90dfba8e6e15d573c1eeb
kr805566 <62494588+kr805566@users.noreply.github.com>
2023-11-01 06:53:33 +0800

    Add files via upload

---
+++ sin及cos繪製/sin及cos繪製/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(619, 223)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 288)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 32)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "sin"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(277, 288)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 32)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "cos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(665, 399)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ sin及cos繪製/sin及cos繪製/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim p As Graphics = PictureBox1.CreateGraphics()
+        Dim x, y, h As Integer
+        Dim i As Double
+
+        h = PictureBox1.Height / 2                 '垂直中心
+        p.DrawLine(Pens.Black, 0, 0, 0, h * 2)     '垂直線
+        p.DrawLine(Pens.Black, 0, h, 800, h)       '水平線
+
+
+        For i = 0 To 3.1415926535897931 * 2 Step 0.01    ' 0 到 2*pi
+            x = Int(i * 100)                             ' x座標
+            y = Int(Math.Sin(i) * -100) + h              ' y座標
+
+            p.DrawRectangle(Pens.Blue, x, y, 1, 1)       '繪製(x,y)
+        Next i
+
+        MsgBox(Math.Acos(Math.Cos(Math.PI)) / Math.PI * 180)
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim p As Graphics = PictureBox1.CreateGraphics()
+        Dim x, y, h As Integer
+        Dim i As Double
+
+        h = PictureBox1.Height / 2
+        p.DrawLine(Pens.Black, 0, 0, 0, h * 2)
+        p.DrawLine(Pens.Black, 0, h, 800, h)
+
+
+        For i = 0 To 3.1415926535897931 * 2 Step 0.01
+            x = Int(i * 100)
+            y = Int(Math.Cos(i) * -100) + h
+
+            p.DrawRectangle(Pens.Red, x, y, 1, 1)
+        Next i
+    End Sub
+
+    
+ 
+End Class
